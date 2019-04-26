817af3b7646c8cba2f3e42f0cb50785c0086c104
yakir-jak <46091043+yakir-jak@users.noreply.github.com>
2019-04-26 19:39:36 +0300

    add checkk user script
    ff

---
+++ check_user_9.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+###########################################################################################
+#
+# Created By : Yakir BST
+# Purpose : Check If User Exist And Get His Details
+# Date : Mon Apr 22 16:38:49 IDT 2019
+# Version : 0.0.1
+#
+###########################################################################################
+echo "this script will check if a user name exist and guve all details about : "
+
+sleep 1
+
+echo "Type user name you want to check :"
+
+read user
+
+details=$(id $user)
+
+uid=$(id -u $user)
+
+all_uid=$(cat /etc/passwd |awk -F : '{print $3}')
+
+exist=0
+for i in $all_uid;
+do
+
+		if [ $i = $uid ] ; then
+
+			echo " The user $user exist !!"
+			
+			exist=1			
+
+			break
+
+#		else 	
+#			echo "user $user is not exist !!"
+
+		fi 2> /dev/null
+
+done
+
+if [ $uid -ge 500 -a $uid -le 1000 ]; then
+
+	echo "$details"
+
+	else exit
+
+fi 2> /dev/null
